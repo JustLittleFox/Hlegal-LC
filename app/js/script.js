e8308c17e15f2421ea5fb8352fa60da8b400748f
JustLittleFox <13fox.fox.fox.13@gmail.com>
2023-06-13 19:55:17 +0200

    html css js fixed

--- app/js/script.js
+++ app/js/script.js
@@ -3,5 +3,28 @@ const menu = document.querySelector('.js-menu-list');
 
 btn.addEventListener('click', function () {
     menu.classList.toggle('active');
+    btn.classList.toggle('close');
 })
+const buttons = document.querySelectorAll(".js-exp-btn");
+
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        buttons.forEach(function (otherButton) {
+            otherButton.classList.remove("active");
+        });
+        this.classList.add("active");
+    });
+});
+const langLinks = document.querySelectorAll('.js-lang-toggle');
+
+langLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        langLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
+
 
